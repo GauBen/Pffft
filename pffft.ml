f035e723f590da2c3a21e86b21eabeffad95d80c
GauBen <benaim.gautier@gmail.com>
2021-01-10 15:45:40 +0100

    style: renamed argument

--- pffft.ml
+++ pffft.ml
@@ -18,19 +18,19 @@ let forall_bool () =
 let forsome_bool () =
   Delimcc.shift pt (fun cont -> try cont true with Invalid -> cont false)
 
-let rec forall flux =
-  match Flux.uncons flux with
+let rec forall values =
+  match Flux.uncons values with
   | Some (v, suite) -> if forall_bool () then v else forall suite
   | None -> miracle ()
 
-let rec forsome flux =
-  match Flux.uncons flux with
+let rec forsome values =
+  match Flux.uncons values with
   | Some (v, suite) -> if forsome_bool () then v else forsome suite
   | None -> failure ()
 
-let rec foratleast n flux =
+let rec foratleast n values =
   if n <= 0 then miracle ();
-  match Flux.uncons flux with
+  match Flux.uncons values with
   | Some (v, suite) -> (
       match
         Delimcc.shift pt (fun cont ->
