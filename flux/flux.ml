71cf0b389bbd75d02923332bc4b2320ea1c5f739
GauBen <benaim.gautier@gmail.com>
2021-01-19 15:49:48 +0100

    style: fixed formatting

--- flux/flux.ml
+++ flux/flux.ml
@@ -8,31 +8,30 @@ let uncons (Tick flux) = Lazy.force flux
 
 let rec apply f x =
   Tick
-    ( lazy
-      ( match (uncons f, uncons x) with
+    (lazy
+      (match (uncons f, uncons x) with
       | None, _ -> None
       | _, None -> None
-      | Some (tf, qf), Some (tx, qx) -> Some (tf tx, apply qf qx) ) )
+      | Some (tf, qf), Some (tx, qx) -> Some (tf tx, apply qf qx)))
 
 let rec unfold f e =
   Tick
-    ( lazy
-      (match f e with None -> None | Some (t, e') -> Some (t, unfold f e')) )
+    (lazy
+      (match f e with None -> None | Some (t, e') -> Some (t, unfold f e')))
 
 let rec filter p flux =
   Tick
-    ( lazy
-      ( match uncons flux with
+    (lazy
+      (match uncons flux with
       | None -> None
-      | Some (t, q) -> if p t then Some (t, filter p q) else uncons (filter p q)
-      ) )
+      | Some (t, q) -> if p t then Some (t, filter p q) else uncons (filter p q)))
 
 let rec append flux1 flux2 =
   Tick
-    ( lazy
-      ( match uncons flux1 with
+    (lazy
+      (match uncons flux1 with
       | None -> uncons flux2
-      | Some (t1, q1) -> Some (t1, append q1 flux2) ) )
+      | Some (t1, q1) -> Some (t1, append q1 flux2)))
 
 let constant c = unfold (fun () -> Some (c, ())) ()
 
