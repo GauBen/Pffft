8fcf9e01acedcd50f9ac4e7b97a36f25cb266e94
GauBen <benaim.gautier@gmail.com>
2021-01-19 19:31:26 +0100

    test: checking code given as example

--- tests/example.ml
+++ tests/example.ml
@@ -8,12 +8,12 @@ let _ =
     Pffft.check
       Pffft.(
         fun () ->
-          (* Pour tout entier n dans [3; 99] :*)
+          (* Pour tout entier n dans [3; 99] : *)
           let n = forall (range 3 99) in
           (* tel que n impair : *)
           assumption (fun () -> n mod 2 = 1);
           on_success (fun () -> Format.printf "%d est premier.@." n);
-          (* Pour tout entier p dans [2; n-1] :*)
+          (* Pour tout entier p dans [2; n-1] : *)
           let p = forall (range 2 (n - 1)) in
           on_failure (fun () ->
               Format.printf "Contre-exemple : %d divise %d.@." p n);
@@ -21,3 +21,61 @@ let _ =
           assertion (fun () -> n mod p <> 0))
   then failwith "Le théorème est juste jusqu'à 99."
   else print_endline "Le théorème est faux."
+
+let%test _ =
+  let is_lower c = c = Char.lowercase_ascii c in
+  not
+    (Pffft.check
+       Pffft.(
+         fun () ->
+           let c = forall (Flux.of_list [ 'h'; 'e'; 'y'; 'H'; 'i' ]) in
+           assertion (fun () -> is_lower c)))
+
+let%test _ =
+  let is_lower c = c = Char.lowercase_ascii c in
+  Pffft.check
+    Pffft.(
+      fun () ->
+        let c = forsome (Flux.of_list [ 'H'; 'E'; 'Y'; 'h'; 'I' ]) in
+        assertion (fun () -> is_lower c))
+
+let%test _ =
+  let is_prime n =
+    let rec aux n i =
+      if i * i > n then true else n mod i <> 0 && aux n (i + 1)
+    in
+    n >= 2 && aux n 2
+  in
+  Pffft.check
+    Pffft.(
+      fun () ->
+        let a = forall (range 2 20) in
+        if is_prime a then
+          let b = foratleast (a - 2) (range 1 a) in
+          assertion (fun () -> a mod b <> 0)
+        else
+          let b = foratleast 3 (range 1 a) in
+          assertion (fun () -> a mod b = 0))
+
+let%test _ =
+  let is_prime n =
+    let rec aux n i =
+      if i * i > n then true else n mod i <> 0 && aux n (i + 1)
+    in
+    n >= 2 && aux n 2
+  in
+  Pffft.check
+    Pffft.(
+      fun () ->
+        let a = forall (range 2 6) in
+        if is_prime a then (
+          on_success (fun () ->
+              Format.printf "%d est premier et n'a que 2 diviseurs.@." a);
+          let b = foratleast (a - 2) (range 1 a) in
+          assertion (fun () -> a mod b <> 0))
+        else (
+          on_success (fun () ->
+              Format.printf "%d n'est pas premier et a au moins 3 diviseurs.@."
+                a);
+          let b = foratleast 3 (range 1 a) in
+          assertion (fun () -> a mod b = 0)))
